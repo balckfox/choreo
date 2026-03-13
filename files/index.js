0b83d35565a18eca82d30a9acedc324c9c1c58f5
balckfox <sunny997@protonmail.com>
2026-03-13 03:30:01 +0000

    Update index.js

--- files/index.js
+++ files/index.js
@@ -1,7 +1,7 @@
 const express = require("express");
 const app = express();
 const { exec, execSync } = require('child_process');
-const port = process.env.SERVER_PORT || process.env.PORT || 3000;        
+const port = process.env.SERVER_PORT || process.env.PORT || 5000;        
 const UUID = process.env.UUID || 'b6f3102c-49a0-4df3-bc4b-9bea62cead31'; //若需要改UUID，需要在config.json里改为一致
 const NEZHA_SERVER = process.env.NEZHA_SERVER || '';     
 const NEZHA_PORT = process.env.NEZHA_PORT || '';                     // 哪吒端口为{443,8443,2096,2087,2083,2053}其中之一开启tls
